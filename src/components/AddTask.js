39f52b6ad72544075e7a08a9803f3b44e661388d
Tony-bytes-0 <79365044+Tony-bytes-0@users.noreply.github.com>
2022-07-05 17:35:40 -0400

    Add files via upload

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
-const base = {'display':'flex', 'width':'35%', 'height':'60%','padding':'1.5vw', 
-'background':'#6B22FF','flexDirection':'column','alignItems':'center','marginTop':'3%'}
+const base = {'display':'flex', 'width':'35%', 'height':'60%','padding':'0.7vw', 
+'background':'#6B22FF','flexDirection':'column','alignItems':'center','marginTop':'1%'}
 const inner = {'display':'flex', 'width':'100%', 'height':'40%', 'background':'#936EB0',
 'flexDirection':'column','alignItems':'center','margin':'2%','padding':'3vw'}
 const smallInner = {'display':'flex', 'width':'100%', 'height':'30%', 'background':'#936EB0',
 'flexDirection':'column','alignItems':'center','margin':'2%','padding':'3vw'}
 const btn = {'display':'flex','flexDirection':'row','alignItems':'center','margin':'5px','padding':'10px'}
-const TaskStyles = {'display':'flex', 'width':'55%', 'height':'90%','padding':'1.5vw', 
-'background':'#6B22FF','flexDirection':'column','alignItems':'center','marginTop':'3%'}
+const TaskStyles = {'display':'flex', 'width':'55%', 'height':'80%','padding':'1.5vw', 
+'background':'#6B22FF','flexDirection':'column','alignItems':'center','marginTop':'1%'}
 const btnBox = {'display':'flex','flexDirection':'row','alignItems':'center'}
 const btns = {'margin':'1vw'}
 
@@ -18,8 +18,8 @@ const modalEdit ={'visibility':'hidden','display':'flex','position':'absolute','
 'alignItems':'center','width':'100%', 'height':'100%','padding':'1.5vw','zIndex':'200','background':'#2353BB'}
 const viewTittle = {'display':'flex','width':'90%','justifyContent':'center'}
 class SameMethods extends Component{
-    viewAllTasks = async function (url){
-        fetch(url)
+    viewAllTasks = async function (){
+        fetch(this.state.baseUrl)
         .then(response => response.json())
         .then(data => {let x = [];let ALLDATA = []
             data.forEach(element => {x.push([element.name, element.description, element.id])});
@@ -30,18 +30,13 @@ class SameMethods extends Component{
 }
 class New extends SameMethods {
     state = {largeView:false,baseUrl:"http://127.0.0.1:3939/api/tasks"}
-    delete = async function(id){console.log(id)
-        //document.getElementById(child).remove()//accion vieja
-        fetch(this.state.baseUrl + '/' + id,{ "method":"DELETE"})
-        document.getElementById('fetchBtn').click()//debo arreglar esto luego
-    }
     viewHide = function(rute){
         let x = rute
         if(x.style.visibility === 'visible')x.style.visibility = 'hidden';
         else{x.style.visibility = 'visible'}
     }
     captureInput = function(param){
-        let x = document.getElementById(param).value
+        let x = document.getElementById(param).value;//console.log(x)
         return x
     }
     editContent = function(content, param){
@@ -50,6 +45,28 @@ class New extends SameMethods {
     clearInput = function(param){
         document.getElementById(param).value = ""
     }
+    delete = async function(id){//console.log(id)
+        fetch(this.state.baseUrl + '/' + id,{ "method":"DELETE"})
+        .then(response => document.getElementById('fetchBtn').click() )//debo arreglar esto luego
+        
+    }
+    fetchPlaceholders = function(url, id){
+        fetch(url + '/' + id)
+        .then(response => response.json())
+        .then(data => {
+            this.editContent(data.name, id +'modalInputTittle');
+            this.editContent(data.description, id +'modalInputDescription')
+        })
+    }
+    updateTask = function (id, Name, description){
+        let data = {
+            "method":"PUT",
+            "body":JSON.stringify({"name":Name,"description":description}),
+            "headers": {'Content-Type': 'application/json'}
+        }
+        fetch(this.state.baseUrl + '/' + id, data)
+        .then(response => document.getElementById('fetchBtn').click() )//debo arreglar esto luego
+    }
     
     render(){return(<>
                     <div id={this.props.ID + 'view'} style={view} className='container rounded '>
@@ -57,40 +74,43 @@ class New extends SameMethods {
                         <p id ={this.props.ID +'description'} style={{'display':'flex'}}>{this.props.description}</p>
 
                         <div id={this.props.ID + 'modalEdit'} style={modalEdit}>
-                            <textarea id={this.props.ID + 'input'}
-                            style={{'display':'flex','width':'100%','height':'100%','padding':'4vw','margin':'5%'}}></textarea>
+
+                            <textarea id={this.props.ID + 'modalInputTittle'}
+                                style={{'display':'flex','width':'100%','height':'30%','padding':'0.5vw','margin':'1%'}}>
+                            </textarea>
+                            <textarea id={this.props.ID + 'modalInputDescription'}
+                                style={{'display':'flex','width':'100%','height':'30%','padding':'0.5vw','margin':'1%'}}>
+                            </textarea>
+                            
                             <div style={{'display':'flex','justifyContent':'center','alignItems':'center'}} 
                             className='container align-self-end d-grid gap-2'>
                                 <button style={btns} className='btn btn-primary'
                                     onClick={() => {
-                                    this.editContent(this.captureInput(this.props.ID + 'input'), this.props.ID +'Name');
-                                    this.editContent(this.captureInput(this.props.ID + 'input'), this.props.ID +'h1Name');
-                                    this.clearInput(this.props.ID + 'input')
                                     this.viewHide(document.getElementById(this.props.ID+'modalEdit'))
-                                }}>Edit Tittle</button>
-
-                                <button style={btns} className='btn btn-primary'
-                                    onClick={() => {
-                                    this.editContent(this.captureInput(this.props.ID + 'input'), this.props.ID +'description');
-                                    this.clearInput(this.props.ID + 'input')
-                                    this.viewHide(document.getElementById(this.props.ID+'modalEdit'))
-                                }}>Edit Description</button>
+                                    let tittle = this.captureInput(this.props.ID +'modalInputTittle')
+                                    let description = this.captureInput(this.props.ID +'modalInputDescription')
+                                    this.updateTask(this.props.ID, tittle, description)
+                                }}>Submit</button>
 
                                 <button style={btn} className='btn btn-warning' 
                                     onClick ={() => {
                                     this.viewHide(document.getElementById(this.props.ID+'modalEdit'));
-                                    this.clearInput(this.props.ID + 'input')
+                                    //this.clearInput(this.props.ID + 'modalInputTittle')//para limpiar el campo
+                                    //this.clearInput(this.props.ID + 'modalInputDescription')//para limpiar el campo
                                 }}>Cancel</button>
                             </div>
                         </div>
 
-                        <div style={{'display':'flex','justifyContent':'center','alignItems':'center'}}
+                        <div style={{'display':'flex','justifyContent':'center','alignItems':'center'}}//
                         className='container align-items-center'>
 
-                            <button style={btns} onClick={() => {this.viewHide(document.getElementById(this.props.ID+'view'))}}
-                            className='btn btn-primary'>Minimize</button>
+                            <button style={btns} className='btn btn-primary' 
+                                onClick={() => {this.viewHide(document.getElementById(this.props.ID+'view'))
+                            }} >Minimize</button>
+
                             <button style={btns} className='btn btn-primary'
-                            onClick={() => {this.viewHide(document.getElementById(this.props.ID+'modalEdit'))
+                                onClick={() => {this.fetchPlaceholders(this.state.baseUrl,this.props.ID)//aqui se colocan los datos de la task
+                                this.viewHide(document.getElementById(this.props.ID+'modalEdit'))//en el input
                             }}>Edit</button>
 
                         </div>
@@ -122,6 +142,9 @@ class AddTask extends SameMethods {
         keys:1,
         baseUrl:"http://127.0.0.1:3939/api/tasks"
     }
+    componentDidMount(){
+        this.viewAllTasks()
+    }
     captureData = function(){
         let x = document.getElementById('inputTaskDescription').value
         let y = document.getElementById('inputTaskName').value
@@ -137,7 +160,7 @@ class AddTask extends SameMethods {
             "headers": {'Content-Type': 'application/json'}
         }
         fetch(url, data)
-        this.viewAllTasks(this.state.baseUrl)
+        .then(prom => this.viewAllTasks())
     }
 
     render() { 
@@ -162,18 +185,11 @@ class AddTask extends SameMethods {
                         onClick={() => {
                         let x = this.captureData()//esta es la lista con los datos capturados de los inputs
                         this.send(x,this.state.baseUrl)
-
-
                     }}>Add</button>
 
                     <button style={btn} className='btn btn-warning col-5' onClick ={() => {
                     this.captureData();//para borrar los inputs
                     }}>Cancel</button>
-
-                    <button id='fetchBtn' style={btn} className='btn btn-warning col-5' onClick ={() => {
-                    this.viewAllTasks(this.state.baseUrl)
-                    }}>Force Server Update #DEV</button>
-
 
                 </div>
 
